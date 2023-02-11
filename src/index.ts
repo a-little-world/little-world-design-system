f193d336fbb974f8c3d05fdfc1c85d9b8256d626
Sean Blundell <seanblundell@gmail.com>
2023-02-11 18:27:09 +0100

    remove webpack and build with tsc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+export * from './components/Button/Button';
+export * from './components/Card/Card';
+export * from './components/Icon/Icon';
+export * from './components/Label/Label';
 export * from './components/Modal/Modal';
+export * from './components/ProgressBar/ProgressBar';
+export * from './components/RadioGroup/RadioGroup';
+export * from './components/Text/Text';
+export * from './components/TextInput/TextInput';
 
 export const App = () => 'boo';
