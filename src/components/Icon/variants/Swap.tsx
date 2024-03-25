4abac79bfeceefb2d326d53ff983eaa496a64e54
Simba14 <seanblundell@gmail.com>
2024-03-25 14:00:29 +0100

    add more icons and fix some issues with TextArea

--- src/components/Icon/variants/Swap.tsx
+++ src/components/Icon/variants/Swap.tsx
@@ -45,12 +45,11 @@ export const SwapIcon = (props: IconSvgProps) => {
       >
         <path
           d="M11.5 3.57143L1.5 3.57143M1.5 3.57143L4.22727 6.14286M1.5 3.57143L4.22727 1M1.5 10.4286L11.5 10.4286M11.5 10.4286L8.77273 13M11.5 10.4286L8.77273 7.85714"
-          fill={gradient ? `url(#gradient${id})` : color}
+          stroke={gradient ? `url(#gradient${id})` : color}
           strokeWidth="1.5"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
-
         {gradient && <IconGradient variation={gradient} id={id} />}
       </svg>
     </Icon>
