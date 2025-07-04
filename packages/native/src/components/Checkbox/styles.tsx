a1e2da2d006835160f0fd897669d76168ef6cf51
Nico Theodorakopoulos <nico.theodorakopoulos@capgemini.com>
2025-07-04 14:00:13 +0200

    fixed checkbox

--- packages/native/src/components/Checkbox/styles.tsx
+++ packages/native/src/components/Checkbox/styles.tsx
@@ -4,51 +4,32 @@ import { DefaultTheme } from 'styled-components/native';
 
 const ITEM_WIDTH = 16;
 
-// export const CheckboxButtonContainer = styled(Checkbox.Root)<{
-//   $hasError?: boolean;
-// }>`
-//   cursor: pointer;
-//   padding: ${({ theme }) => theme.spacing.xxsmall};
-//   display: flex;
-//   align-items: center;
-//   border: 1px solid
-//     ${({ theme, checked, $hasError }) =>
-//       checked
-//         ? $hasError
-//           ? theme.color.border.error
-//           : theme.color.border.selected
-//         : theme.color.border.subtle};
-//   border-radius: ${({ theme }) => theme.radius.xxsmall};
-//   background: ${({ checked, $hasError, theme }) =>
-//     checked
-//       ? $hasError
-//         ? theme.color.surface.error
-//         : theme.color.surface.accent
-//       : theme.color.surface.secondary};
-
-//   label {
-//     cursor: pointer;
-//   }
-// `;
-
 export const getContainerStyles = ({ theme }: { theme: DefaultTheme }) => StyleSheet.create({
   container: {
     display: 'flex',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     marginVertical: theme.spacing.xxxxsmall,
+    flexDirection: 'row',
   }
 })
 
 export const getCheckboxStyles = ({ theme, hasError, color, checked }: { theme: DefaultTheme, hasError: boolean, color?: string, checked: boolean }) => StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  text: {
+    paddingHorizontal: 8
+  },
   checkbox: {
-    color: theme.color.text.primary,
-    width: ITEM_WIDTH,
     height: ITEM_WIDTH,
-    display: 'flex',
+    width: ITEM_WIDTH,
+    borderWidth: 1,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: theme.radius.xxsmall,
-    borderWidth: 1,
+    display: 'flex',
     borderColor: hasError ? theme.color.border.error : theme.color.border.contrast,
     backgroundColor: checked ? color : theme.color.surface.primary,
     ...(checked && color && {
@@ -67,6 +48,3 @@ export const indicatorStyles = StyleSheet.create({
   }
 });
 
-// export const StyledLabel = styled(Label)`
-//   margin-left: ${({ theme }) => theme.spacing.xxsmall};
-// `;
