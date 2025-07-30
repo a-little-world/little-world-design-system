dc5a929827a9631cff2ea31763f612bd4b0c243b
Simba14 <seanblundell@gmail.com>
2025-07-30 17:22:23 +0200

    fix issues with font loading in web

--- packages/native/src/utils/createReactNativeSvg.tsx
+++ packages/native/src/utils/createReactNativeSvg.tsx
@@ -22,6 +22,8 @@ import {
 } from "@a-little-world/little-world-design-system-core";
 import IconGradient from "../components/Icon/IconGradient";
 
+const BLACK_HEX = '#000';
+
 // Helper function to generate unique keys
 const generateUniqueKey = (element: SvgElement, index: string | number) => {
   return `${element.type}-${index}-${Math.random()
@@ -54,14 +56,14 @@ const renderSvgElement = (
   // Handle color attributes for SVG elements based on the colorAttribute property
   // If element has color set that isn't #000 - do not override
   if (element.colorAttribute && element.colorAttribute !== "none") {
-    if (attrs[element.colorAttribute] === "#000") {
-      attrs[element.colorAttribute] = color || "currentColor";
+    if (attrs[element.colorAttribute] === BLACK_HEX) {
+      attrs[element.colorAttribute] = color || BLACK_HEX;
     } else {
       if (gradient) {
         attrs[element.colorAttribute] = `url(#gradient${gradientId})`;
       } else {
         attrs[element.colorAttribute] =
-          attrs[element.colorAttribute] || color || "currentColor";
+          attrs[element.colorAttribute] || color || BLACK_HEX;
       }
     }
   }
