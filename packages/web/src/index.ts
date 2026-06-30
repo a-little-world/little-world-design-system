721706ede6363c8a5c73e5050bf4485d6d925f99
sureshmani-accenture <suresh.b.mani@accenture.com>
2026-06-30 15:35:19 +0530

    conflict resolve

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -66,8 +66,8 @@ export {
 export { default as FileUploader } from './components/FileUploader/FileUploader';
 export type { FileUploaderProps } from './components/FileUploader/FileUploader';
 export { default as Modal } from './components/Modal/Modal';
-export { default as Table } from './components/Table/Table';
-export type { TableProps, Column, SortDirection } from './components/Table/Table';
+export { default as PDFViewer } from './components/PDFViewer/PDFViewer';
+export type { PDFViewerProps } from './components/PDFViewer/PDFViewer';
 export {
   default as CheckboxGroup,
   type CheckboxGroupProps,
